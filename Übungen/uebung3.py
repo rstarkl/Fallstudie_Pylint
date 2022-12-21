fed7edd25273d916bbbd512d13d1fea571870bf7
rstarkl <120170164+rstarkl@users.noreply.github.com>
2022-12-21 18:33:14 +0100

    Delete uebung3.py

--- Übungen/uebung3.py
+++
@@ -1,9 +0,0 @@
-# Die Teilnehmer sollten Pylint auf den folgenden Code anwenden und alle Probleme beheben, die gefunden werden.
-
-def calculateSum(numbers):
-    sum = 0
-    for number in numbers:
-        sum += number
-    print(sum)
-
-calculateSum([1, 2, 3, 4, 5])
