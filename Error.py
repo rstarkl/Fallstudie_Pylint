6f4267033a75a48bbad34df9ae378fb0f48d3360
odaub@stud.hs-heilbronn.de <1532Oliver>
2022-12-21 17:44:20 +0100

    Hallo

--- Error.py
+++ Error.py
@@ -1,5 +1,6 @@
 # Pylint wird eine Ausgabe machen, weil die Variable nicht definiert wurde
 def someFunction():
+        x += 1
     print(x)
 
 # Pylint wird eine Ausgabe machen, weil die Funktion nicht existiert
