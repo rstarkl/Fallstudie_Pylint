44b3d81ff2cd8d09f45e94c72327fa4b8a3ef133
rstarkl <120170164+rstarkl@users.noreply.github.com>
2022-12-22 18:16:13 +0100

    Update Error.py

--- Error.py
+++ Error.py
@@ -1,8 +1,7 @@
-# Pylint wird eine Ausgabe machen, weil die Variable nicht definiert wurde
+"HALLO"
 def someFunction():
         x += 1
     print(x)
 
-# Pylint wird eine Ausgabe machen, weil die Funktion nicht existiert
 def someFunction():
     nonExistentFunction()
