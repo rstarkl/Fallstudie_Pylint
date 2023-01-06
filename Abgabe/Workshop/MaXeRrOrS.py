a963e435c6b2e003ee26c05e83fd57539a5e3bda
rstarkl <120170164+rstarkl@users.noreply.github.com>
2023-01-06 14:09:15 +0100

    Add files via upload

---
+++ Abgabe/Workshop/MaXeRrOrS.py
@@ -0,0 +1,6 @@
+def dashieristeinefunktiondieeinenvielzulangennamenhat(variable, x):
+    y = None 
+    if x is x:     
+        y =x+variable   
+        print(y) #Das hier ist ein viel zu langer Kommentar für diese eine Zeile um die erlaubten 79 Zeichen pro Zeile zu erfüllen
+dashieristeinefunktiondieeinenvielzulangennamenhat(10, 10)  
